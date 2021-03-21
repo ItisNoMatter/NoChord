4449c78d0f08577c3171d38490346af25e0500b2
702sunnylife <41831807+702sunnylife@users.noreply.github.com>
2021-03-21 18:13:17 +0900

    Add files via upload
    ハッカソン個人開発で一気に書き上げたいろいろです

---
+++ gunjo.py
@@ -0,0 +1,11 @@
+from  no_chord import play
+
+Ds = "D# G A#"
+F  = "F A C"
+D  = "D F# A"
+Gm = "G B- D"
+DonFs = "F# D F# A"
+
+gunjo_codes = [Ds,F,D,DonFs,Gm,Ds,F,Ds,F]
+while 1:
+    play(gunjo_codes)
