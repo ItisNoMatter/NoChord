57a06e1d3b1b2e8bdc0c443f83ecbd7d51164f73
ItisNoMatter <702sunnylife@gmail.com>
2021-05-07 11:31:18 +0900

    done audio_test

--- study_kivy/audio_test/audio_test.py
+++ study_kivy/audio_test/audio_test.py
@@ -1,13 +1,12 @@
 import kivy
-kivy.require('1.9.0')
-
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.core.audio import SoundLoader
 
-class HelloApp(App):
-
+class music(App):
+    sound = SoundLoader.load('pianoC4.wav')
     def build(self):
-        return Label(text='Hello World')
-
-if __name__ == '__main__':
-    HelloApp().run()
+        return Label(text='music playing')
+    if sound:
+        sound.play()
+music().run()
