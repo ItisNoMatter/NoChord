2d07a46fe3e80630b6e95e17051f7679af133000
wanwan0622 <minnie20000622@yahoo.co.jp>
2021-07-04 21:32:38 +0900

    add AutamnLeavs.py

---
+++ AutumnLeavs.py
@@ -0,0 +1,13 @@
+from  no_chord import play
+
+Cm7 = "C E- G B-"
+F7 = "F A C E-"
+BbM7 = "B- D F A"
+EbM7 = "E- G B- D"
+Am7 = "A C E- G"
+D7 = "D F# A C"
+Gm = "G B- D"
+
+autamn_leavs_codes = [Cm7, F7, BbM7, EbM7, Am7, D7, Gm, Cm7, F7, BbM7, EbM7, Am7, D7, Gm, Am7, D7, Gm, Cm7, F7, BbM7, EbM7, Am7, D7, Gm, F7, EbM7, D7, Gm]
+while 1:
+    play(autamn_leavs_codes)
